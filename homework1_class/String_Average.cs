a350a85904401fd244db31f663e36aa8c458197a
s9904320 <91059581+s9904320@users.noreply.github.com>
2022-01-09 22:02:23 +0800

    Add project files.

---
+++ homework1_class/String_Average.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace homework1_class
+{
+    public  class StringAverage
+    {
+        public static string AverageString(string str)
+        {
+            Dictionary<int, string> myDic = new Dictionary<int, string>();
+            myDic.Add(0, "zero");
+            myDic.Add(1, "one");
+            myDic.Add(2, "two");
+            myDic.Add(3, "three");
+            myDic.Add(4, "four");
+            myDic.Add(5, "five");
+            myDic.Add(6, "six");
+            myDic.Add(7, "seven");
+            myDic.Add(8, "eight");
+            myDic.Add(9, "nine");
+
+            if (!String.IsNullOrEmpty(str))
+            {
+                string[] num = str.Split(' ');
+                int numResult = 0;
+                foreach (string strNum in num)
+                {
+                    foreach(KeyValuePair<int,string> kvr in myDic)
+                        if (strNum == kvr.Value)
+                    {
+                            numResult += kvr.Key;
+                    }
+                }
+
+                numResult = numResult / num.Length;
+
+                if (numResult > 9 || numResult < 0)
+                {
+                    str = "n/a";
+                }
+                else
+                {
+                    foreach (KeyValuePair<int, string> kvr in myDic)
+                        if (numResult == kvr.Key)
+                        {
+                            str = kvr.Value;
+                        }
+                }
+            }
+            else
+            {
+                str = "n/a";
+            }
+            return str;
+        }
+    }
+}
---
+++ homework1_classTests/StringAverageTests.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using homework1_class;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework1_class.Tests
+{
+    [TestClass()]
+    public class StringAverageTests
+    {
+        [TestMethod()]
+        public void AverageStringTest()
+        {
+            //Arrange
+            string[] actual = {
+                null,
+                "",
+                "zero nine five two",
+                "four six two three",
+                "one two three four five",
+                "five four",
+                "zero zero zero zero zero",
+                "one one eight one"
+            };
+            string[] trueAnswer = {
+            "n/a",
+            "n/a",
+            "four",
+            "three",
+            "three",
+            "four",
+            "zero",
+            "two"
+            };
+            //Act
+            string[] answer = new string[actual.Length];
+            for (int i = 0; i < actual.Length; i++)
+            {
+                answer[i] = StringAverage.AverageString(actual[i]);
+            }
+            //Assert
+            for (int i = 0; i < actual.Length; i++)
+            {
+                Assert.AreEqual(trueAnswer[i], answer[i]);
+            }
+        }
+
+        [TestMethod]
+        public void testNullOrEmpty()
+        {
+            Assert.AreEqual("n/a", StringAverage.AverageString(null));
+            Assert.AreEqual("n/a", StringAverage.AverageString(""));
+        }
+        [TestMethod]
+        public void testZero()
+        {
+            Assert.AreEqual("zero", StringAverage.AverageString("zero zero zero zero zero"));
+        }
+        [TestMethod]
+        public void testNormal()
+        {
+            Assert.AreEqual("four", StringAverage.AverageString("zero nine five two"));
+            Assert.AreEqual("three", StringAverage.AverageString("one two three four five"));
+        }
+        [TestMethod]
+        public void testFloor()
+        {
+            Assert.AreEqual("three", StringAverage.AverageString("four six two three")); 
+            Assert.AreEqual("four", StringAverage.AverageString("five four"));
+            Assert.AreEqual("two", StringAverage.AverageString("one one eight one")); 
+        }
+    }
+}
